779b8ff854b628bf3dfc095e5507636404d0f1ab
Mauro Iorio <mio@bit4id.com>
2025-01-07 15:53:28 +0100

    fix: change error handling in pages-range

--- src/pdfpwd/utils.py
+++ src/pdfpwd/utils.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from argparse import ArgumentError
 from itertools import chain
 
 
@@ -9,6 +8,6 @@ def parse_range_list(rl):
             return []
         parts = r.split("-")
         if len(parts) > 2:
-            raise ArgumentError(f'Invalid range: {r}')
+            raise ValueError(f'Invalid range: {r}')
         return range(int(parts[0]), int(parts[-1])+1)
     return sorted(set(chain.from_iterable(map(parse_range, rl.split(",")))))
